a3cfb3ee8e3604fbb916a7b63bb7dec2e741c01f
NemesisUA <display.n0ne.01.gmail.com>
2024-06-10 13:56:21 +0300

    feat: hide user name and Logout button on login and registration routes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
 
 import Logo from './components/Logo/Logo';
@@ -12,6 +12,9 @@ const Header = () => {
 	const navigate = useNavigate();
 	const { user, signOut, token } = useAuth();
 
+	let { pathname } = useLocation();
+	let hideUserAndLogout = pathname === '/login' || pathname === '/registration';
+
 	function handleLogout() {
 		signOut(() => navigate('/login'));
 	}
@@ -19,10 +22,10 @@ const Header = () => {
 	return (
 		<header className={styles.header}>
 			<Logo />
-			{!token && (
+			{!token && !hideUserAndLogout && (
 				<Button buttonText='Login' onClick={() => navigate('/login')} />
 			)}
-			{token && (
+			{token && !hideUserAndLogout && (
 				<div>
 					<b style={{ marginRight: '40px' }}>{formatUserName(user.name)}</b>
 					<Button buttonText='Logout' onClick={handleLogout} />
